805cefdf1b67c9c1760e6df6390d3afd30e9b9c7
Benjamin DALSASS <benjamin.dalsass@combodo.com>
2026-04-21 09:21:44 +0200

    N°8688 - Remove WebPage::add_linked_stylesheet and WebPage::add_linked_script methods

--- src/Hook/OCSngStylesheets.php
+++ src/Hook/OCSngStylesheets.php
@@ -5,43 +5,13 @@
  */
 
 
-if (interface_exists('iBackofficeLinkedStylesheetsExtension')) {
-	class OCSngStylesheets implements iBackofficeLinkedStylesheetsExtension
-	{
+class OCSngStylesheets implements iBackofficeLinkedStylesheetsExtension
+{
 
-		public function GetLinkedStylesheetsAbsUrls(): array
-		{
-			$sScssFile = utils::GetAbsoluteUrlModulesRoot().'itop-ocsng/asset/css/style.css';
+    public function GetLinkedStylesheetsAbsUrls(): array
+    {
+        $sScssFile = utils::GetAbsoluteUrlModulesRoot().'itop-ocsng-extensions/asset/css/style.css';
 
-			return [$sScssFile];
-		}
-	}
-} else {
-	class OCSngStylesheets implements iPageUIExtension
-	{
-		/**
-		 * @inheritdoc
-		 */
-		public function GetNorthPaneHtml(\iTopWebPage $oPage)
-		{
-			// SCSS files can't be loaded asynchroniously before of a bug in the output() method prior to iTop 2.6
-			$oPage->add_linked_stylesheet(Utils::GetAbsoluteUrlModulesRoot().'itop-ocsng/asset/css/style.css');
-		}
-
-		/**
-		 * @inheritdoc
-		 */
-		public function GetSouthPaneHtml(\iTopWebPage $oPage)
-		{
-			// Do nothing.
-		}
-
-		/**
-		 * @inheritdoc
-		 */
-		public function GetBannerHtml(\iTopWebPage $oPage)
-		{
-			// Do nothing.
-		}
-	}
+        return [$sScssFile];
+    }
 }
